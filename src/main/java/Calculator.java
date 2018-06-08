b944ed7d3a71c98ab5ddaf3f35996a344836590f
MichaelPetrusevich <ip.petrusevich@gmail.com>
2018-06-08 11:45:37 +0300

    update

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
     public static void main(String[] args) {
 
         int num1=10;
-        int num2=2;
+        int num2=5;
 
         System.out.println("Результат операции сложения числа "+num1+" с числом "+num2+ ": "+addition(num1,num2));
         System.out.println("Результат операции вычитания числа "+num1+" с числом "+num2+ ": "+subtraction(num1,num2));
@@ -34,6 +34,9 @@ public class Calculator {
     public static float division (float num1,float num2){
         float result;
         result=num1/num2;
+        if (num2 == 0){
+            throw new IllegalArgumentException("Number can not be divide by 0!");
+        }
         return result;
     }
 }
